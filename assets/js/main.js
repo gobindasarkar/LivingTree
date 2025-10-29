a539893f7d74f63c9890574c7b8b95d75960da89
gobinda sarkar <55000159+gobindasarkar@users.noreply.github.com>
2025-10-29 11:55:53 +0530

    update all

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@
 
   $(window).on("scroll", function () {
     const scrolled = $(this).scrollTop() >= 300;
-    $(".header-aera").toggleClass("stick", scrolled);
+    $(".header-area").toggleClass("stick", scrolled);
   });
 
   jQuery(document).ready(function () {
@@ -28,7 +28,39 @@
     new VenoBox({
       selector: '.my-video-links',
     });
-    
+    /*-------------------------------------------
+    one page nave active
+  --------------------------------------------- */
+    $(function () {
+      const $menuLinks = $('nav a');
+      const headerHeight = $('header').outerHeight();
+      $menuLinks.on('click', function (e) {
+        e.preventDefault();
+
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - headerHeight
+          }, 600, 'swing');
+        }
+        $menuLinks.removeClass('active');
+        $(this).addClass('active');
+      });
+      $(window).on('scroll', function () {
+        const scrollPos = $(window).scrollTop();
+
+        $('section').each(function () {
+          const top = $(this).offset().top - headerHeight - 1;
+          const bottom = top + $(this).outerHeight();
+          const id = $(this).attr('id');
+
+          if (scrollPos >= top && scrollPos < bottom) {
+            $menuLinks.removeClass('active');
+            $menuLinks.filter('[href="#' + id + '"]').addClass('active');
+          }
+        });
+      });
+    });
 
 
   });
